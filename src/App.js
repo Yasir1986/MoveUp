6f8e78d7d24f9258908538e9217e9b633b9ce37f
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-07-19 23:21:43 +0300

    css

--- src/App.js
+++ src/App.js
@@ -3,37 +3,49 @@ import "./App.css";
 
 function App() {
   const [top, setTop] = useState(100);
-  const  [right, setRight] = useState(100);
+  const [right, setRight] = useState(100);
 
+  
   function moveUp() {
-    setTop(top - 50);
+    setTop(top - 10);
   }
 
   function moveDown() {
-    setTop(top + 50);
+    setTop(top + 10);
+  }
+ 
+  function moveRight() {
+    setRight(right + 10);
   }
 
-  function moveRight(){
-      setRight(right + 10)
+  function moveLeft() {
+    setRight(right - 10);
   }
-
-  function moveLeft(){
-    setRight(right - 10)
-}
 
   return (
     <div className="App">
       <h1>MOVE ME</h1>
 
-      <p><button onClick={moveUp}>Move Up</button></p> 
+      <p>
+        <button onClick={moveUp}>Up</button>
+      </p>
+      <p>
+        <button onClick={moveDown}>Down</button>
+      </p>
 
-      <p><button onClick={moveDown}>Move Down</button></p>
-
-       <p><button onClick={moveRight}>Move Right</button></p>
-       <p><button onClick={moveLeft}>Move Left</button></p>
-
-      <div className="box" style={{transform:`translateY(${top}px)`}} />
-      <div className="box1" style={{transform:`translateX(${right}px)`}} />
+    
+        <div className="box" style={{ transform: `translateY(${top}px) ` }} />
+        <div className="box1" style={{ transform: `translateX(${right}px)` }} />
+    
+        
+      <div class="btn-down">
+        <p>
+          <button onClick={moveRight}>Right</button>
+        </p>
+        <p>
+          <button onClick={moveLeft}>Left</button>
+        </p>
+      </div>
     </div>
   );
 }
